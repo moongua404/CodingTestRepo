8592d3fd5b57bff26a093da18aa5f5c9c0acc23d
Hogun Park <162525421+moongua404@users.noreply.github.com>
2024-12-04 15:04:05 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 192 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, a, b;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a >> b;
+        cout << a+b << "\n";
+    }
+}
