f69b7a6bfb62fc2f05d9dc599457b705e9d7674b
Hogun Park <162525421+moongua404@users.noreply.github.com>
2024-12-04 14:25:20 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    cout << (A+B)%C << "\n";
+    cout << ((A%C) + (B%C))%C << "\n";
+    cout << (A*B)%C << "\n";
+    cout << ((A%C) * (B%C))%C << "\n";
+}
