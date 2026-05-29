2cd2172190012c1623600c8b2eb46d99c4a55cd2
Hogun Park <162525421+moongua404@users.noreply.github.com>
2026-05-29 13:33:39 +0900

    [level 3] Title: 카운트 다운, Time: 6.33 ms, Memory: 4 MB -BaekjoonHub

--- 프로그래머스/3/131129. 카운트 다운/카운트 다운.cpp
+++ 프로그래머스/3/131129. 카운트 다운/카운트 다운.cpp
@@ -1,12 +1,6 @@
 /*
-싱글 [1...20]
-더블 [2,4...40]
-트리플 [3,6...60]
-불 [50]
-각 요소를 선택했지만 optimul을 맞출 수 없다면 최대를 선택
-
-pair<int, int> [다트 수] [싱글/불의 수]
-dfs -> bfs
+dp
+121 * 100,000
 */
 
 #include <string>
@@ -14,66 +8,42 @@ dfs -> bfs
 
 using namespace std;
 
-struct Node {
-    int left;
-    pair<int, int> trial; // [total][싱글/불]
-    
-    Node(int left, pair<int, int> trial) : left(left), trial(trial) { }
-    
-    Node add(int left, pair<int, int> trial) {
-        return Node(
-            this->left + left, 
-            {this->trial.first+trial.first, this->trial.second+trial.second});
-    }
-};
-
 bool isLessThan(pair<int, int> a, pair<int, int> b) {
     if (a.first == b.first) return a.second > b.second;
     return a.first < b.first;
 }
 
+pair<int, int> add(pair<int, int> base, int f, int s) {
+    return {base.first + f, base.second + s};
+}
+
 vector<int> solution(int target) {
-    vector<Node> q; // 가짜임 사실 스택임
-    q.push_back(Node(target, {0, 0}));
-    pair<int, int> min = {0x7FFFFFFF, 0x7FFFFFFF};
-    while(!q.empty()) {
-        Node node = q.back();
-        q.pop_back();
-        if (isLessThan(min, node.trial)) {
-            continue;
-        }
-        if (node.left == 0) {
-            min = node.trial;
-            continue;
-        }
+    vector<pair<int, int>> v(target+1); // []를 만들 때 최소의 비용
+    
+    for (int i = 1; i <= target; i++) {
+        pair<int, int> min = {0x7FFFFFFF, 0x7FFFFFFF};
         
-        // 하아드코딩..
-        if (node.left >= 3) { // 트리플
-            int triple = node.left;
-            if (triple > 60) triple = 60;
-            if (triple % 3 != 0) triple -= (triple % 3);
-            q.push_back(node.add(-triple, {1, 0}));
+        for (int j = i-20; j < i; j++) {
+            if (j < 0) continue;
+            pair<int, int> c = add(v[j], 1, 1);
+            if (isLessThan(c, min)) min = c;
         }
-        
-        if (node.left >= 300) {
-            continue; // 충분히 크다면 이왕이면 트리플
+        for (int j = i-40; j < i; j += 2) {
+            if (j < 0) continue;
+            pair<int, int> c = add(v[j], 1, 0);
+            if (isLessThan(c, min)) min = c;
         }
-        
-        if (node.left >= 50) { // 불
-            q.push_back(node.add(-50, {1, 1}));
+        for (int j = i-60; j < i; j += 3) {
+            if (j < 0) continue;
+            pair<int, int> c = add(v[j], 1, 0);
+            if (isLessThan(c, min)) min = c;
         }
-        
-        if (node.left >= 2) { // 더블
-            int doub = node.left;
-            if (doub > 40) doub = 40;
-            if (doub % 2 == 1) doub--;
-            q.push_back(node.add(-doub, {1, 0}));
-        }
-        
-        int single = node.left; // 싱글
-        if (single > 20) single = 20;
-        q.push_back(node.add(-single, {1, 1}));
+        if (i-50 >= 0) {
+            pair<int, int> c = add(v[i-50], 1, 1);
+            if (isLessThan(c, min)) min = c;
+        }        
+        v[i] = min;
     }
     
-    return vector<int>{min.first, min.second};
+    return {v[target].first, v[target].second};
 }
