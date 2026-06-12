ef4d576f4780711fefba8fafa6bbc9b23faa7e28
Hogun Park <162525421+moongua404@users.noreply.github.com>
2026-06-12 11:12:43 +0900

    [level 3] Title: 숫자 야구, Time: 2965.12 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/451808. 숫자 야구/README.md
+++ 프로그래머스/3/451808. 숫자 야구/README.md
@@ -1,10 +1,10 @@
 # [level 3] 숫자 야구 - 451808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/451808) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/451808#) 
 
 ### 성능 요약
 
-메모리: 4.2 MB, 시간: 1.67 ms
+메모리: 4.2 MB, 시간: 2965.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 12일 10:39:59
+2026년 06월 12일 11:12:41
 
 ### 문제 설명
 
--- 프로그래머스/3/451808. 숫자 야구/숫자 야구.cpp
+++ 프로그래머스/3/451808. 숫자 야구/숫자 야구.cpp
@@ -1,8 +1,5 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-
-#include <iostream>
 
 using namespace std;
 
@@ -17,51 +14,58 @@ vector<int> split(int v) {
     return vec;
 }
 
-// freq size should 10
-int pick(vector<int>& vec, vector<int>& freq) {
-    int bestV = 9999;
-    int bestS = 0x7FFFFFFF;
-    for (int v : vec) {
-        vector<int> res = split(v);
-        sort(res.begin(), res.end());
-        res.erase(unique(res.begin(), res.end()), res.end());
-        int score = 10000;
-        for (int r : res) {
-            score /= 10;
-            score *= (freq[r]+1);
+pair<int, int> validate(int answer, int submitted) {
+    vector<int> parsed = split(answer);
+    vector<int> parsedBase = split(submitted);
+    int vStrike = 0, vBall = 0;
+    for (int i = 0; i < 4; i++) {
+        if (parsed[i] == parsedBase[i]) {
+            vStrike++;
+            continue;
         }
-        if (score < bestS) {
-            bestS = score;
+        for (int j = 0; j < 4; j++) {
+            if (parsed[j] == parsedBase[i]) {
+                vBall++;
+                break;
+            }
+        }
+    }
+    return {vStrike, vBall};
+}
+
+int pick(vector<int>& vec) {
+    int bestV; // 최적의 값
+    int bestC = 0x7FFFFFFF; // 그 때 최악의 경우의 수
+    
+    for (int v : vec) {
+        int worst = 0;
+        for (int strike = 0; strike <= 4; strike++) {
+            for (int ball = 0; ball <= 4; ball++) {
+                if (strike + ball > 4) continue;
+                int cases = 0;
+                for (int o : vec) {
+                    pair<int, int> res = validate(o, v);
+                    if (res.first == strike && res.second == ball) cases++;
+                }
+                if (cases > worst) worst = cases;
+            }
+        }
+        if (worst < bestC) {
+            bestC = worst;
             bestV = v;
         }
     }
+    
     return bestV;
-}
-
-pair<int, int> parseToResult(string res) {
-    return {res[0] - '0', res[3] - '0'};
 }
 
 vector<int> filter(vector<int>& vec, int base, int strike, int ball) {
     vector<int> newVec;
     
-    vector<int> parsedBase = split(base);
     for(int& v : vec) {
         if (v == base) continue;
-        vector<int> parsed = split(v);
-        int vStrike = 0, vBall = 0;
-        for (int i = 0; i < 4; i++) {
-            if (parsed[i] == parsedBase[i]) vStrike++;
-            else {
-                for (int j = 0; j < 4; j++) {
-                    if (parsed[j] == parsedBase[i]) {
-                        vBall++;
-                        break;
-                    }
-                }
-            }
-        }
-        if (vStrike == strike && vBall == ball) {
+        pair<int, int> res = validate(v, base);
+        if (res.first == strike && res.second == ball) {
             newVec.push_back(v);
         }
     }
@@ -72,21 +76,20 @@ int solution(int n) {
     vector<int> vec;
     vec.reserve(9000);
     for (int i = 1000; i <= 9999; i++) {
+        vector<int> t = split(i);
+        if (t[0] == t[1] || t[0] == t[2] || t[0] == t[3]
+            || t[1] == t[2] || t[1] == t[3] || t[2] == t[3]) continue;
         vec.push_back(i);
     }
     
-    vector<int> freq(10, 0);
-    freq[0] = 1;
     for (int _ = 0; _ <= 30; _++) {
         if (vec.size() == 1) return vec[0];
-        int v = pick(vec, freq);
+        int v = 1234;
+        if (_ != 0) v = pick(vec);
         string response = submit(v);
         if (response == "4S 0B") return v;
-        pair<int, int> result = parseToResult(response);
+        pair<int, int> result = {response[0] - '0', response[3] - '0'};
         vec = filter(vec, v, result.first, result.second);
     }
-    
     return 0;
 }
-
-// 6304
